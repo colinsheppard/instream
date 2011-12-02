c89d9144dce919d55d64ed9cc8cda7cb7fa30e53
Colin Sheppard <colinthegaunt@gmail.com>
2011-12-02 14:20:33 -0800

    TroutObserverSwarm now mirrors TroutBatchSwarm w.r.t. optional output files (closes #51)

--- TroutObserverSwarm.m
+++ TroutObserverSwarm.m
@@ -120,76 +120,40 @@ Boston, MA 02111-1307, USA.
    [probeMap setProbedClass: [FishCell class]];
    probeMap = [probeMap createEnd];
 
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 1\n");
-  fflush(0);
   [probeMap addProbe: [probeLibrary getProbeForVariable: "polyCellNumber"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 2\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "polyCellDepth"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 3\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "polyCellVelocity"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 4\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "numberOfFish"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 5\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "polyCellArea"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 6\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "cellFracSpawn"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 7\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "cellDistToHide"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 8\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "cellShelterArea"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 9\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "shelterAreaAvailable"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 10\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "driftHourlyCellTotal"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 11\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "hourlyAvailDriftFood"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 12\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "searchHourlyCellTotal"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 13\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForVariable: "hourlyAvailSearchFood"
                       inClass: [FishCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 14\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForMessage: "tagPolyCell"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 15\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForMessage: "unTagPolyCell"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 16\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForMessage: "tagAdjacentCells"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 17\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForMessage: "unTagAdjacentCells"
                       inClass: [PolyCell class]]];
-  fprintf(stdout, "TroutObserverSwarm >>>> buildProbesIn >>>> 18\n");
-  fflush(0);
    [probeMap addProbe: [probeLibrary getProbeForMessage: "tagCellsWithin:"
                       inClass: [FishCell class]]];
    
@@ -334,6 +298,19 @@ Boston, MA 02111-1307, USA.
   //troutModelSwarm->rasterResolutionX = rasterResolutionX;
   //troutModelSwarm->rasterResolutionY = rasterResolutionY;
   //troutModelSwarm->rasterColorVariable = rasterColorVariable;
+
+  [troutModelSwarm setWriteFoodAvailabilityReport: writeFoodAvailabilityReport];
+  [troutModelSwarm setWriteDepthReport: writeDepthReport];
+  [troutModelSwarm setWriteVelocityReport: writeVelocityReport];
+  [troutModelSwarm setWriteHabitatReport: writeHabitatReport];
+  [troutModelSwarm setWriteDepthVelocityReport: writeDepthVelocityReport];
+  [troutModelSwarm setWriteMoveReport: writeMoveReport];
+  [troutModelSwarm setWriteReadyToSpawnReport: writeReadyToSpawnReport];
+  [troutModelSwarm setWriteSpawnCellReport: writeSpawnCellReport];
+  [troutModelSwarm setWriteReddSurvReport: writeReddSurvReport];
+  [troutModelSwarm setWriteCellFishReport: writeCellFishReport];
+  [troutModelSwarm setWriteReddMortReport: writeReddMortReport];
+  [troutModelSwarm setWriteIndividualFishReport: writeIndividualFishReport];
 
   //
   // Instantiate the objects first;
