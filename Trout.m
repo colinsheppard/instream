edcde19e8727b9b8209572ee77069346cc87e1e5
SteveRailsback <Steve@LangRailsback.com>
2013-10-07 08:01:43 -0700

    Fix mistake in selecting male spawner- condition > min. Closes #81

--- Trout.m
+++ Trout.m
@@ -1191,7 +1191,7 @@ Boston, MA 02111-1307, USA.
    {
        return NO;
    }
-   if([aTrout getFishCondition] > fishParams->fishSpawnMinCond) 
+   if([aTrout getFishCondition] < fishParams->fishSpawnMinCond) 
    {
        return NO;
    }
